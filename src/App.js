722638f1b9ab6938d332e9b221a30644d29d3bb4
ek.natan <ek.natan@gmail.com>
2023-05-01 15:43:51 +0300

    update image public

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-import myImage from '/home/n/Documents/test_git/test1/src/1.jpeg';
+import myImage from './images/1.jpeg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"  />
         <p>
           Welcome to <h1>Test Git</h1>
           repository!!
         </p>
         <div>
-      <img src={myImage} alt="My Image" 
-      style={{ width: '1700px', height: '1000px' }}
-      />
-    </div>
-        
+          <img src={myImage} alt="There was supposed to be a picture here" 
+            style={{ width: '1700px', height: '1000px' }}
+          />
+        </div>
       </header>
     </div>
   );
